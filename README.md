3ac2aeca7bc192ec88dd209b71e50b5cdbb40735
RITHIGA SRI B <rithigasri383@gmail.com>
2021-11-28 11:46:55 +0530

    Updated Aim and Algorith

--- README.md
+++ README.md
@@ -1,58 +1,24 @@
 # Experiment_Time_Table
 
-## AIM
-To Write a html webpage page to display your timetable.
+## AIM:
+To Write a html webpage page to Display First Semester TimeTable.
 
-# ALGORITHM
-### STEP 1
-create a simple table using table tag
-### STEP 2
-Add header row using th tag
-### STEP 3
-Add your timetable
-### STEP 4
+# ALGORITHM:
+
+### STEP 1:
+Create a Simple Table using Table Tag
+
+### STEP 2:
+Add Header row using th tag
+
+### STEP 3:
+Add first Semester TimeTable
+
+### STEP 4:
 Execute the program
 
-# CODE
-~~~<!DOCTYPE html>
-<html>
-
-   <head>
-      <title>TIME TABLE</title>
-   </head>
-	
-   <body>
-      <table border = "1" cellspacing="1" bordercolor="blue" bgcolor="yellow">
-         <tr>
-            <th colspan="8">TIME TABLE</th>
-         </tr>
-         
-         <tr>
-            <th>DAYS</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-             <th>4</th>
-            <th rowspan="6">lunch break</th>
-            <th>5</th>
-            <th>6</th>
-         </tr>
-          <tr>
-             <td>MONDAY</td>
-             <td>Accounts</td>
-             <td>English</td>
-             <td>Statistics</td>
-             <td>Banking</td>
-             <td align="center">-</td>
-             <td align="center">EP</td>
-         </tr>
-  
-         
-      </table>
-      
-   </body>
-</html>
+# CODE:
 
 
-~~~
-# OUPUT
+# OUPUT:
+
---
+++ format.py
@@ -0,0 +1,77 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>TIMETABLE</title>
+</head>
+<body style="background-color:lavender">
+<img src="https://www.saveetha.ac.in/images/WEB_LOGO-01.png" width="100%" height="200">
+<h1 style="text-align:center;">FIRST SEMESTER TIMETABLE</h1>
+
+<table border="1" cellspacing="1" bordercolor="black" bgcolor="lavenderblush" width="100%">
+    
+    <tr>
+        <th colspan="8" height="40px" style="background-color:lightcyan">TIME TABLE</th>
+    </tr>
+    <tr bgcolor="honeydew">
+        <th colspan="4" height="40px">NAME:RITHIGA SRI.B</th>
+        <th colspan="8" height="40px">REFERENCE NO:21500732</th>
+    </tr>
+    <tr height="40px">
+        
+        <th style="background-color:yellow">DAY</th>
+        <th style="background-color:yellow">1</th>
+        <th style="background-color:yellow">2</th>
+        <th style="background-color:yellow">3</th>
+        <th style="background-color:yellow">4</th>
+        <th rowspan="6" bgcolor="cornsilk">LUNCH BREAK</th>
+        <th style="background-color:yellow">5</th>
+        <th style="background-color:yellow">6</th>
+    </tr>
+    <tr height="40px" style="text-align:center;">
+        <td style="background-color:palegreen">MONDAY</td>
+        <td colspan="2">Fundamentals of Web Technology</td>
+        <td colspan="2">Linear Algebra Laboratory</td>
+        <td colspan="2">Mathematics for Artificial Intelligence</td>
+    </tr>
+    <tr height="40px" style="text-align:center;">
+        <td style="background-color:palegreen">TUESDAY</td>
+        <td colspan="2">Free Time</td>
+        <td colspan="2">Engineering Design and Modeling</td>
+        <td colspan="2">Engineering Mechanics and Product Development</td>
+    </tr>
+    <tr height="40px" style="text-align:center;">
+        <td style="background-color:palegreen">WEDNESDAY</td>
+        <td colspan="2">Soft Skills</td>
+        <td colspan="2">Python Programming</td>
+        <td colspan="2">Fundamentals of Web Technology</td>
+    </tr>
+    <tr height="40px" style="text-align:center;">
+        <td style="background-color:palegreen">THURSDAY</td>
+        <td colspan="2">Engineering Mechanics and Product Development</td>
+        <td colspan="2">Python Programming</td>
+        <td colspan="2">Engineering Design and Modeling</td>
+    </tr>
+    <tr height="40px" style="text-align:center;">
+        <td style="background-color:palegreen">FRIDAY</td>
+        <td colspan="2">Free Time</td>
+        <td colspan="2">Mathematics for Artificial Intelligence</td>
+        <td colspan="2">Web Technology Laboratory</td>
+    </tr>
+
+</table>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8080)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
